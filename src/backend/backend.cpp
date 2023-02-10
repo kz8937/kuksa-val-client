91fe9e7989aa3f95e237607153a1ca3ec6758d3f
payam <payam@debian-BULLSEYE-live-builder-AMD64>
2023-02-10 15:23:31 +0100

    feat: Dirty solurtion.

--- src/backend/backend.cpp
+++ src/backend/backend.cpp
@@ -1,29 +1,66 @@
-#include <iostream>
-#include <iostream>
-#include <string>
-int main()
+// C++ Code generated from Python Code:
+class Backend : public cli_backend::Backend
 {
-    bool done = false;
-    std::string input{};
-    while (!done)
+public:
+    Backend(const cli_backend::Config &config) : cli_backend::Backend(config)
     {
-        std::cout << "Enter command: ";
-        std::getline(std::cin, input);
-        if (input == "quite")
-        {
-            done = true;
-        }
-        else if (input == "help")
-        {
-            std::cout << "\nCommand List:\n"
-                      << "help: Display this help text \n"
-                      << "quite: Exist program\n"
-                      << std::endl;
-        }
-        else
-        {
-            std::cout << "Unrecognized Command" << std::endl;
-        }
+        wsConnected = false;
+        run = false;
     }
-    return 0;
-}
+
+    void stop()
+    {
+        wsConnected = false;
+        run = false;
+        std::cout << "Server disconnected." << std::endl;
+    }
+
+    void authorize(const std::string &token)
+    {
+        if (wsConnected)
+        {
+            std::cout << "Already connected to server." << std::endl;
+            return;
+        }
+
+        std::string url = "wss://" + config.host + ":" + std::to_string(config.port) + "/api/v1/ws";
+        std::cout << "Connecting to " << url << std::endl;
+
+        try
+        {
+            websocketpp::lib::error_code ec;
+            client.init_asio();
+            client.set_access_channels(websocketpp::log::alevel::none);
+            client.set_open_handler(bind(&Backend::on_open, this, ::_1));
+            client.set_close_handler(bind(&Backend::on_close, this, ::_1));
+            client.set_message_handler(bind(&Backend::on_message, this, ::_1, ::_2));
+            client.set_tls_init_handler(bind(&Backend::on_tls_init, this, ::_1));
+            client.set_fail_handler(bind(&Backend::on_fail, this, ::_1));
+            client.set_proxy("http://127.0.0.1:8888");
+            client.set_proxy_basic_auth("user", "password");
+            client.set_proxy_ntlm_auth("user", "password");
+            client.set_proxy_digest_auth("user", "password");
+            client.set_proxy_negotiate_auth("user", "password");
+            client.set_proxy_bearer_auth("user", "password");
+            client.set_proxy_oauth2_auth("user", "password");
+            client.set_proxy_type(websocketpp::proxy_type::socks5);
+            client.set_proxy_type(websocketpp::proxy_type::socks4);
+            client.set_proxy_type(websocketpp::proxy_type::http);
+            client.set_proxy_type(websocketpp::proxy_type::none);
+            client.set_proxy_type(websocketpp::proxy_type::basic);
+            client.set_proxy_type(websocketpp::proxy_type::ntlm);
+            client.set_proxy_type(websocketpp::proxy_type::digest);
+            client.set_proxy_type(websocketpp::proxy_type::negotiate);
+            client.set_proxy_type(websocketpp::proxy_type::bearer);
+            client.set_proxy_type(websocketpp::proxy_type::oauth2);
+            client.set_proxy_type(websocketpp::proxy_type::unknown);
+            client.set_proxy_type(websocketpp::proxy_type::any);
+            client.set_proxy_type(websocketpp::proxy_type::all);
+            client.set_proxy_type(websocketpp::proxy_type::default);
+            client.set_proxy_type(websocketpp::proxy_type::default_http);
+            client.set_proxy_type(websocketpp::proxy_type::default_socks5);
+            client.set_proxy_type(websocketpp::proxy_type::default_socks4);
+            client.set_proxy_type(websocketpp::proxy_type::default_basic);
+            client.set_proxy_type(websocketpp::proxy_type::default_ntlm);
+            client.set_proxy_type(websocketpp::proxy_type::default_digest);
+            client.set_proxy_type(websocketpp::proxy_type::default_neg
